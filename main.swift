e7425998099e7c4ac6ddfb02b76cc7efea2ff6ff
Robel Abraham <robel.abraham@student.allenisd.org>
2022-10-05 15:27:25 -0500

    Adding all base files

---
+++ main.swift
@@ -0,0 +1,31 @@
+
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let unsortedIntegers = [5, 1, 4, 2, 8]
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+
+import Foundation
+
+var newArray = unsortedIntegers
+var swapCount = 0
+var totalSwapCount = 0
+var passCount = 0
+
+print("Pass: \(passCount), Swaps: \(swapCount)/\(totalSwapCount), Array: \(newArray)")
+for x in 1..<newArray.count {
+    var y = x
+    while y > 0 && newArray[y] < newArray[y - 1] {
+        newArray.swapAt(y-1,y)
+        y -= 1
+        swapCount += 1
+    }
+    passCount += 1
+    totalSwapCount += swapCount
+    print("Pass: \(passCount), Swaps: \(swapCount)/\(totalSwapCount), Array: \(newArray)")
+    swapCount = 0
+}
